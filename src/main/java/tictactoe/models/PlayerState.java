1dc12e39d656f34599858382ccb6558da86fd6b5
dev_martin <martin.y@inception.asia>
2021-05-17 17:29:02 +0700

    push model

--- src/main/java/tictactoe/models/PlayerState.java
+++ src/main/java/tictactoe/models/PlayerState.java
@@ -1,4 +1,24 @@
 package tictactoe.models;
 
-public class PlayerState {
+public enum PlayerState {
+    IN_PROGRESS,
+    WIN,
+    LOS,
+    DRAW;
+
+    public boolean isInProgress(){
+        return this == IN_PROGRESS;
+    }
+    public boolean isWin(){
+        return this == WIN;
+    }
+    public boolean isLos(){
+        return this == LOS;
+    }
+    public boolean isDraw(){
+        return this == DRAW;
+    }
+    public boolean isGameOver(){
+        return this == WIN || this == LOS || this == DRAW;
+    }
 }
